b4d572f162011989fe31da9a4de21076f453e242
Matteo Cosenza <matteocosenza12@gmail.com>
2022-05-31 17:54:18 +0200

    cd-98 (creata parte logica gestione form)

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+username:string;
+password:string;
+formLogin;
 
-  constructor() { }
+
+  constructor(private fb:FormBuilder) {
+    this.formLogin=this.fb.group({
+      username: ['',Validators.required, Validators.email],
+      password:['',Validators.required,Validators.min(4)],
+  })
+   }
 
   ngOnInit() {
   }
