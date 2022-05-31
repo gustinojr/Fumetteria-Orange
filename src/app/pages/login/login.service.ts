0f60a41750c2d6779a0a35640e95e7da519c0bfd
Michele Imparato <micheleimparato96@gmail.com>
2022-05-31 18:04:18 +0200

    Aggiunti i servizi a login

---
+++ src/app/pages/login/login.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { UserCommand } from 'src/app/models/user/user-command';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { }
+  
+  login( email: string, password: string) {
+    
+    const user = new UserCommand;
+    user.email = email;
+    user.password = password;
+
+    return this.http.post<any>("http://2.44.173.210:7080/comic-be/api/login/login", user); 
+
+  }
+
+}
