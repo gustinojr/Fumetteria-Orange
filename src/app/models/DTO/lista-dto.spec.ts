0ede4a18d3bea53800756f945341b56254522fbc
gustinojr <dagostinoraf@gmail.com>
2022-06-07 16:42:07 +0200

    carosello corretto

--- src/app/models/DTO/lista-dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ListaDto } from './lista-dto';
-
-describe('ListaDto', () => {
-  it('should create an instance', () => {
-    expect(new ListaDto()).toBeTruthy();
-  });
-});
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FumettiDto } from 'src/app/models/DTO/fumetti-dto';
 import { ListaService } from '../../home.service';
 import { ListaFumettiDto } from '../../models/DTO/lista-dto';
 
@@ -8,32 +9,41 @@ import { ListaFumettiDto } from '../../models/DTO/lista-dto';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
-bottom=false;
-top=true;
-fumetti:ListaFumettiDto;
- sliderConfig = {
-  spaceBetween: 10,
-  centeredSlides: true,
-  slidesPerView:1.6,
-  autoplay: true
-};
+export class HomePage implements OnInit {
+  bottom = false;
+  top = true;
+  fumetti: ListaFumettiDto;
 
- cliccato(){
-  this.bottom=!this.bottom;
-  this.top=!this.top;
- }
+  info =
+    [{
+    Descrizione: "Questo è un bel fumetto;",
+    Genere: "Comic;",
+    Autore: "Pippo;",
+    Categoria: "Misc;"
+}];
 
-  constructor(private service:ListaService, private router:Router) {
-    this.fumetti=new ListaFumettiDto;
+  sliderConfig = {
+    spaceBetween: 10,
+    centeredSlides: true,
+    slidesPerView: 1.6,
+    autoplay: true
+  };
+
+  cliccato() {
+    this.bottom = !this.bottom;
+    this.top = !this.top;
+  }
+
+  constructor(private service: ListaService, private router: Router) {
+    this.fumetti = new ListaFumettiDto;
   }
 
   ngOnInit() {
     this.service.listaFumetti().subscribe((resp) => {
       this.fumetti = resp;
     });
-    
-   
-     
+
+
+
   }
 }
