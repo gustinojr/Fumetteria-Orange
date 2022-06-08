83825da54799e506f9f0dd8f1297f162cfc384da
gustinojr <dagostinoraf@gmail.com>
2022-06-08 18:27:46 +0200

    user info

--- src/app/home.service.ts
+++ src/app/home.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { UserDTO } from './models/user/user-dto';
+import { ActivatedRoute } from '@angular/router';
+import { UserInfo } from './models/user/user-info';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +11,8 @@ import { HttpClient } from '@angular/common/http';
 export class ListaService {
 
   constructor(private http:HttpClient) { }
+
+  
 
   listaFumetti(){
     return this.http.post("http://2.44.173.210:7080/comic-be/api/comic/search",{});
