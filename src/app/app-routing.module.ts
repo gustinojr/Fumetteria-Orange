de35caea428f6b5ab00314e8e39374ab3a49e30b
gustinojr <dagostinoraf@gmail.com>
2022-06-08 16:22:26 +0200

    aggiunto app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'lista-autori',
-    loadChildren: () => import('./pages/lista-autori/lista-autori.module').then( m => m.ListaAutoriPageModule)
-  },
-{
     path: 'login',
     loadChildren: () =>
       import('./pages/login/login.module').then((m) => m.LoginPageModule),
@@ -26,14 +12,6 @@ const routes: Routes = [
     redirectTo: 'login',
     pathMatch: 'full',
   },
-  {
-    path: 'lista-autori',
-    loadChildren: () =>
-      import('./pages/lista-autori/lista-autori.module').then(
-        (m) => m.ListaAutoriPageModule
-      ),
-  },
-
 
   {
     path: '',
@@ -54,15 +32,40 @@ const routes: Routes = [
     path: 'lista-utenti',
     loadChildren: () => import('./pages/utente/lista-utenti/lista-utenti.module').then( m => m.ListaUtentiPageModule)
   },
+  // {
+  //   path: 'lista-autori',
+  //   loadChildren: () => import('./pages/autori/lista-autori/lista-autori.module').then( m => m.ListaAutoriPageModule)
+  // },
+  // {
+  //   path: 'crea-autori',
+  //   loadChildren: () => import('./pages/autori/crea-autori/crea-autori.module').then( m => m.CreaAutoriPageModule)
+  // },
+  {
+    path: 'crea-categoria',
+    loadChildren: () => import('./pages/categorie/crea-categoria/crea-categoria.module').then( m => m.CreaCategoriaPageModule)
+  },
+  {
+    path: 'lista-categorie',
+    loadChildren: () => import('./pages/categorie/lista-categorie/lista-categorie.module').then( m => m.ListaCategoriePageModule)
+  },
+  {
+    path: 'crea-fumetto',
+    loadChildren: () => import('./pages/fumetti/crea-fumetto/crea-fumetto.module').then( m => m.CreaFumettoPageModule)
+  },
+  {
+    path: 'lista-fumetti',
+    loadChildren: () => import('./pages/fumetti/lista-fumetti/lista-fumetti.module').then( m => m.ListaFumettiPageModule)
+  },
+
+
+
 
 ];
 
-
-
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
